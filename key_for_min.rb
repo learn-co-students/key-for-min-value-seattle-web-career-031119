cded52f27f63a61cda36760245b1786ff6512373
Doug Ward <ward92@gmail.com>
2019-02-18 15:53:32 -0800

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,13 @@
 
 def key_for_min_value(name_hash)
 
+  value_holder = 1000
+  key_holder = nil
+  name_hash.collect do |key, value|
+    if value < value_holder
+      value_holder = value
+      key_holder = key
+    end
+  end
+  key_holder
 end
